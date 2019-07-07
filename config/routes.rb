cc59873cad10520971faae7c587cd246bea2be9b
Kanamen10 <asxsanborn@gmail.com>
2019-07-07 22:24:07 +0900

    update: login/logout function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   get 'session/new'
-  namespace :admin do
+    namespace :admin do
     resources :users
   end
+  
   root to: 'tasks#index'
   resources :tasks
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
